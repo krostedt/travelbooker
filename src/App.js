7ebf48fc0e7501a7de3d0f428d6d7c280d9da40b
Kimmo Rostedt <kimmo@barabra.fi>
2018-02-26 20:46:23 +0200

    Prototyping with react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,72 @@ import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
 import CalendarManager from './CalendarManager';
+import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
+
+const auth = {
+  isAuthenticated: false,
+  login(cb) {
+    this.isAuthenticated = true;
+    //console.log("login");
+    cb();
+  },
+  logout(cb) {
+    this.isAuthenticated = false;
+    cb();
+  }
+};
+
+class Login extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      redirectToCalendar: false,
+      isAuthenticated: false
+    };
+  }
+
+  handleLogin = () => {
+    auth.login(() => {
+      this.setState({ redirectToCalendar: true });
+    });
+  };
+
+  render() {
+    if (this.state.redirectToCalendar) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/calendar',
+            state: { isAuthenticated: this.state.isAuthenticated }
+          }}
+        />
+      );
+    }
+
+    return (
+      <div>
+        <h2>Login</h2>
+        <button onClick={this.handleLogin}>Login</button>
+      </div>
+    );
+  }
+}
+
+const Calendar = () =>
+  !auth.isAuthenticated ? (
+    <Redirect
+      to={{
+        pathname: '/',
+        state: { isAuthenticated: auth.isAuthenticated }
+      }}
+    />
+  ) : (
+    <CalendarManager db={firebase} />
+  );
+
+const Title = () => {
+  return <h1>Travelbooker</h1>;
+};
 
 class App extends Component {
   constructor(props) {
@@ -26,14 +92,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <Title />
-        <CalendarManager db={firebase} />
+        <Route exact path="/" component={Login} />
+        <Route path="/calendar" render={Calendar} />
       </div>
     );
   }
 }
-
-const Title = () => {
-  return <h1>Travelbooker</h1>;
-};
 
 export default App;
